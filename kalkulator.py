4ec916f1c30ce3969a5b405178025c993a3b9845
paulina <64349@student.ue.poznan.pl>
2021-03-30 00:36:29 +0200

    rozgrzebana funkcja dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,4 @@
-def dodaj () :
-    print('Wprowadź dwie liczby a zostaną one dodane.')
-    a = int(input())
-    b = int(input())
-    print (a + b)
+def dodaj (a, b) :
+    wynik = a + b
+    return wynik
 dodaj ()
